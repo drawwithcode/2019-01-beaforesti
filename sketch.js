514a5f6a32793d151b369a1d19e21083c1e16ebf
beaforesti <bea_fiore@live.it>
2019-09-21 19:16:25 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,9 +3,39 @@ function preload(){
 }
 
 function setup() {
-  // put setup code here
+  createCanvas (windowWidth, windowHeight);
+  background(30);
+  angleMode(DEGREES);
+  frameRate(35);
 }
 
 function draw() {
-  // put drawing code here
+  push();
+  translate(width/2, height/2);
+
+// figura centrale
+  stroke(lerpColor(color("yellow"), color("red"),frameCount/120));
+  strokeWeight(3);
+
+  var r = 150
+  line(r, 0, r*cos(frameCount*3), r*sin(frameCount*3));
+
+// cerchi ipnotici
+  stroke(240);
+  noFill();
+  strokeWeight(1);
+
+  ellipse(-width/4, -height/4, r*sin(frameCount*3), r*sin(frameCount*3));
+  ellipse(width/4, -height/4, r*sin(frameCount*3), r);
+  ellipse(width/4, height/4, r*cos(frameCount*3), r*cos(frameCount*3));
+  ellipse(-width/4, height/4, r, r*sin(frameCount*3));
+
+  if (frameCount == 120){
+    noLoop();
+    }
+
+  pop();
+
+
+
 }
